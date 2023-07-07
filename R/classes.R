76b43da874df5ddeab1956e4135cef94a11c5b0b
dariotommasini <dtommasini0@gmail.com>
2023-07-07 10:04:50 -0700

    Updated WGCNA docs

--- R/classes.R
+++ R/classes.R
@@ -1,4 +1,18 @@
-#' @title Class \code{WGCNA}
+#' The WGCNA Class
+#'
+#' The WGCNA class is the main class used in multiWGCNA to store results from 
+#' a weighted gene co-expression nework analysis. These include the original 
+#' unaltered expression data used as input, connectivity metrics, module 
+#' assignment, input sample conditions, trait 
+#'
+#' @slot datExpr The expression data, connectivity data, and module assignment
+#' @slot conditions A data.frame with integer conditions for WGCNA
+#' @slot trait A data.frame showing pearson correlation values to traits
+#' @slot moduleEigengenes A data.frame of module eigengenes for each module across samples
+#' @slot outlierModules A vector of modules classified by our algorithm as being driven by sample outliers
+#'
+#' @return NA
+#'
 #' @import methods
 setClass("WGCNA", slots=list(datExpr="data.frame", 
                              conditions="data.frame", 
